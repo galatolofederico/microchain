ee7e961b397e3c031e0cb7248417b43016106b82
Federico Galatolo <galatolo.federico@gmail.com>
2023-12-20 10:31:48 +0100

    license change

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setuptools.setup(
     ],
     classifiers=[
         "Programming Language :: Python :: 3.11",
-        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "License :: OSI Approved :: Apache Software License",
         "Operating System :: OS Independent",
         "Development Status :: 4 - Beta"
     ],
